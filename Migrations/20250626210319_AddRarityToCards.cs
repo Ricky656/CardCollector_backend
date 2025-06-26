4c32753684149cbd8b74a78440a2aed2f5ec8b60
Alaric Sheer Hardwick <85938018+Alaric656@users.noreply.github.com>
2025-06-26 22:07:18 +0100

    Added Rarity and some validation to card model

---
+++ Migrations/20250626210319_AddRarityToCards.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardCollector_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRarityToCards : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cards",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Rarity",
+                table: "Cards",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rarity",
+                table: "Cards");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Cards",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20);
+        }
+    }
+}
--- Models/Card.cs
+++ Models/Card.cs
@@ -1,7 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CardCollector_backend.Models;
 
 public class Card
 {
+    public enum CardRarity
+    {
+        Common, Uncommon, Rare, Legendary
+    }
     public long Id { get; set; }
-    public string? Name { get; set; }
+
+    [Required]
+    [StringLength(20, MinimumLength=1, ErrorMessage="Card name must be between 1-20 characters")]
+    public string Name { get; set; }
+    public CardRarity Rarity { get; set; }
 }
