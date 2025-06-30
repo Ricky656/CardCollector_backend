fc9cb3c83a49cfb61a25ed6a97b9aa163a7a6b1b
Alaric Sheer Hardwick <85938018+Alaric656@users.noreply.github.com>
2025-06-30 16:37:23 +0100

    add migrations

---
+++ Migrations/20250630153528_AddPacks.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CardCollector_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPacks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Packs",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CardPack",
+                columns: table => new
+                {
+                    CardsId = table.Column<long>(type: "INTEGER", nullable: false),
+                    PacksId = table.Column<long>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CardPack", x => new { x.CardsId, x.PacksId });
+                    table.ForeignKey(
+                        name: "FK_CardPack_Cards_CardsId",
+                        column: x => x.CardsId,
+                        principalTable: "Cards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CardPack_Packs_PacksId",
+                        column: x => x.PacksId,
+                        principalTable: "Packs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CardPack_PacksId",
+                table: "CardPack",
+                column: "PacksId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CardPack");
+
+            migrationBuilder.DropTable(
+                name: "Packs");
+        }
+    }
+}
