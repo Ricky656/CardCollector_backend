4478b78c3b32d2682b3f87e8a7353567c7806fd0
Alaric Sheer Hardwick <85938018+Alaric656@users.noreply.github.com>
2025-07-09 19:19:30 +0100

    Added json enum converter for updating cards

--- Dtos/Card/UpdateCardRequestDto.cs
+++ Dtos/Card/UpdateCardRequestDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using CardCollector_backend.Enums;
 
 namespace CardCollector_backend.Dtos.Cards;
@@ -5,6 +7,10 @@ namespace CardCollector_backend.Dtos.Cards;
 public class UpdateCardRequestDto
 {
     public long Id { get; set; }
+    
+    [StringLength(20, MinimumLength = 1, ErrorMessage = "Card name must be between 1-20 characters")]
     public string Name { get; set; }
+
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public CardRarity Rarity { get; set; }
 }
